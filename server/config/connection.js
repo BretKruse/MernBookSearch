cd03df9fceb85b5227ea54d4f028bad5be24efbd
BretKruse <krusebret@gmail.com>
2023-11-15 18:15:51 -0600

    Atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://krusebret:<Che$$#1993>@cluster0.mkzg9mv.mongodb.net/?retryWrites=true&w=majority');
 
 module.exports = mongoose.connection;
