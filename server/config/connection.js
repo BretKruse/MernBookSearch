db9a020f23277f0db6584711c511060c2a3c1c5c
BretKruse <krusebret@gmail.com>
2023-11-15 18:44:44 -0600

    Atlas updated

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://krusebret:<Che$$#1993>@cluster0.mkzg9mv.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://krusebret:chess@mongo.pduvkat.mongodb.net/?retryWrites=true&w=majority');
 
 module.exports = mongoose.connection;
